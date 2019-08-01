eab19adbedbf5bb49891a8bb5c1cee8212fbf2c0
Max <42402608+Maxteh@users.noreply.github.com>
2019-08-01 10:29:20 +0500

    Update and rename hw6.txt to hw6.java

---
+++ hw6.java
@@ -0,0 +1,13 @@
+6. Íàïèñàòü ìåòîä, êîòîðîìó â êà÷åñòâå ïàðàìåòðà ïåðåäàåòñÿ öåëîå ÷èñëî, ìåòîä äîëæåí âåðíóòü true, åñëè ÷èñëî îòðèöàòåëüíîå;
+
+public class HomeWork_6 {
+    public static void main(String[] args) {
+        boolean a = work(-4);
+        System.out.println(a);
+    }
+        static boolean work(int x){
+        return (0 > x);
+    }
+}
+
+// Код рабочий. Замечаний дополнительных, от озвученых ранее нет.
